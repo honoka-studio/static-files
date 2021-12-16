1672b2b88bd167b01d8ec90c9c5f9181e13809de
Kosaka Bun <kosaka-bun@qq.com>
2021-12-16 14:28:41 +0800

    git init

---
+++ browser/tampermonkey/block_bilibili_search_placeholder.js
@@ -0,0 +1,42 @@
+// ==UserScript==
+// @name         屏蔽bilibili搜索栏推荐
+// @namespace    http://tampermonkey.net/
+// @version      1.0.2
+// @description  try to take over the world!
+// @author       You
+// @match        *://*.bilibili.com/*
+// @icon         https://bilibili.com
+// @grant        none
+// @require      https://cdn.jsdelivr.net/npm/jquery-tampers@3.2.1/jquery.min.js
+// @run-at       document-end
+// @noframes
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    let selector = $('input.nav-search-keyword');
+    let check = function() {
+        //检查选择器长度
+        if(selector.length < 1) {
+            setTimeout(function() {
+                selector = $('input.nav-search-keyword');
+                check();
+            }, 300);
+            return;
+        }
+        //检查搜索框内容
+        let placeholder = selector.attr('placeholder');
+        if(placeholder == '') {
+            setTimeout(function() {
+                check();
+            }, 200);
+            return;
+        }
+        //清除内容
+        selector.attr('title', '');
+        selector.attr('placeholder', '');
+    };
+    check();
+})();
