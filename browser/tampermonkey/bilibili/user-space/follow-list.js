76fc911d592654044d3001ad6b82e83f439ba0c6
Kosaka Bun <kosaka-bun@qq.com>
2022-05-25 02:41:27 +0800

    bilibili关注列表增强脚本更新1.0.2

--- browser/tampermonkey/bilibili/user-space/follow-list.js
+++ browser/tampermonkey/bilibili/user-space/follow-list.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         bilibili个人空间关注列表增强
 // @namespace    http://tampermonkey.net/
-// @version      1.0.1
+// @version      1.0.2
 // @description  bilibili个人空间关注列表增强
 // @author       Kosaka Bun
 // @match        *://space.bilibili.com/*/fans/follow*
@@ -13,13 +13,27 @@
 
 (function() {
     'use strict';
-
-    let followedUserDomList = document.querySelectorAll('.fans-action');
-    for(let i = 1; i <= followedUserDomList.length; i++) {
-        let dom = followedUserDomList[i];
-        let unfollowBtnHtml = `<div class="fans-action-btn follow">
+    let checkAndDoScript = function(count) {
+        setTimeout(function() {
+            if(count > 30) return;
+            let followedUserDomList = document.querySelectorAll(
+                '.fans-action');
+            if(followedUserDomList == null || followedUserDomList.length <= 0) {
+                checkAndDoScript(count + 1);
+                return;
+            }
+            doScript();
+        }, 3 * 100);
+    };
+    let doScript = function() {
+        let followedUserDomList = document.querySelectorAll('.fans-action');
+        for(let i = 1; i <= followedUserDomList.length; i++) {
+            let dom = followedUserDomList[i];
+            let unfollowBtnHtml = `<div class="fans-action-btn follow">
             <span class="fans-action-text my-btn" index="${i}">取关</span>
-            </div>`
-        dom.innerHTML = unfollowBtnHtml + dom.innerHTML;
-    }
+            </div>`;
+            dom.innerHTML = unfollowBtnHtml + dom.innerHTML;
+        }
+    };
+    checkAndDoScript(0);
 })();
