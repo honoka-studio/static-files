6830a778a693a7835a6cae3d847368810f1fbaca
Kosaka Bun <kosaka-bun@qq.com>
2022-05-25 03:46:40 +0800

    bilibili关注列表增强脚本更新1.0.3

--- browser/tampermonkey/bilibili/user-space/follow-list.js
+++ browser/tampermonkey/bilibili/user-space/follow-list.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         bilibili个人空间关注列表增强
 // @namespace    http://tampermonkey.net/
-// @version      1.0.2
+// @version      1.0.3
 // @description  bilibili个人空间关注列表增强
 // @author       Kosaka Bun
-// @match        *://space.bilibili.com/*/fans/follow*
+// @match        *://space.bilibili.com/*
 // @icon         https://www.bilibili.com/favicon.ico
 // @grant        none
 // @run-at       document-end
@@ -27,12 +27,32 @@
     };
     let doScript = function() {
         let followedUserDomList = document.querySelectorAll('.fans-action');
-        for(let i = 1; i <= followedUserDomList.length; i++) {
+        for(let i = 0; i < followedUserDomList.length; i++) {
             let dom = followedUserDomList[i];
-            let unfollowBtnHtml = `<div class="fans-action-btn follow">
-            <span class="fans-action-text my-btn" index="${i}">取关</span>
-            </div>`;
-            dom.innerHTML = unfollowBtnHtml + dom.innerHTML;
+            let unfollowBtnDiv = document.createElement('div');
+            unfollowBtnDiv.setAttribute('class',
+                'fans-action-btn follow');
+            let unfollowBtnSpan = document.createElement('span');
+            unfollowBtnSpan.setAttribute('class',
+                'fans-action-text my-btn');
+            unfollowBtnSpan.innerHTML = '取关';
+            unfollowBtnDiv.appendChild(unfollowBtnSpan);
+            dom.appendChild(unfollowBtnDiv);
+            unfollowBtnDiv.onclick = function() {
+                let dropDownItems = dom.querySelectorAll('.be-dropdown-item');
+                for(let dropDown of dropDownItems) {
+                    if(dropDown.innerText.trim() === '取消关注') {
+                        let event = document.createEvent("HTMLEvents");
+                        event.initEvent('click', true, true);
+                        dropDown.dispatchEvent(event);
+                        unfollowBtnDiv.setAttribute('class',
+                            'fans-action-btn fans-action-follow');
+                        unfollowBtnDiv.setAttribute('style',
+                            'width: 54px;');
+                        unfollowBtnDiv.onclick = undefined;
+                    }
+                }
+            };
         }
     };
     checkAndDoScript(0);
