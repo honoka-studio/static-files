1e54dc41c8f13757b92b3394fb6eaaa3160772f7
Kosaka Bun <kosaka-bun@qq.com>
2023-07-11 12:11:09 +0800

    Add tampermonkey/google/new-tab.js

---
+++ browser/js/tampermonkey/google/new-tab.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         Google搜索结果在新标签页打开
+// @namespace    http://tampermonkey.net/
+// @version      1.0.0
+// @author       Kosaka Bun
+// @match        *://www.google.com/search*
+// @grant        none
+// @run-at       document-end
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    let urlTags = document.querySelectorAll('div#rso a')
+    for(let tag of urlTags) {
+        tag.setAttribute('target', '_blank')
+    }
+})();
