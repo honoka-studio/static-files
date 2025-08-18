1778f85a69700fb61488a468a379f4444504e32f
Kosaka Bun <kosaka-bun@qq.com>
2025-08-18 21:10:46 +0800

    Update browser/js/tampermonkey/github/main.js

--- browser/js/tampermonkey/github/main.js
+++ browser/js/tampermonkey/github/main.js
@@ -2,12 +2,11 @@
 // @name         GitHub基本优化
 // @description  none
 // @namespace    https://github.com/honoka-studio
-// @version      1.0.1
+// @version      1.0.2
 // @author       Kosaka Bun
 // @match        *://*.github.com/*
 // @icon         https://github.com/favicon.ico
 // @grant        none
-// @require      https://raw.githubusercontent.com/honoka-studio/static-files/refs/heads/master/browser/js/honoka-browser-utils/basic.js
 // @run-at       document-end
 // ==/UserScript==
 //noinspection JSIgnoredPromiseFromCall
@@ -15,14 +14,17 @@
 (function() {
   'use strict'
 
-  const utils = window.honokaBrowserUtils
-
   function resetStyles() {
-    utils.basic.setAndClearInterval(() => {
-      let style = document.body.style
-      style.fontFamily = 'var(--fontStack-sansSerif, -apple-system, BlinkMacSystemFont, "Segoe UI", ' +
-        '"Noto Sans", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji")'
-    }, 100, 30)
+    let fontFamily = 'var(--fontStack-sansSerif, -apple-system, BlinkMacSystemFont, "Segoe UI", ' +
+      '"Noto Sans", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji")'
+    let styles = `
+        body {
+            --font-family: ${fontFamily};
+        }
+    `
+    let styleElement = document.createElement('style')
+    styleElement.textContent = styles
+    document.head.appendChild(styleElement)
   }
 
   resetStyles()
